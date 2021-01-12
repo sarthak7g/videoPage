df75efcfcfc7b1a3412a0fb8fdf36d4dce225c8f
deepsingh11421 <deepsingh11421@gmail.com>
2021-01-12 15:40:38 +0530

    Final changes

--- ai-vid_GET/index.js
+++ ai-vid_GET/index.js
@@ -1,14 +1,26 @@
 var QuestionMsg = ''
 var username = ''
 var userEmail = ''
+var videoStream;
+var intervalId;
 document.getElementById("questionText").innerHTML = QuestionMsg;
 
-function detailSubmit(e){
+var uploadFile = document.getElementById("video_file");
+  uploadFile.onchange = () => {
+    console.log('uploaded');
+    formSubmit("yes");
+};
+
+function detailSubmit(e,form='no'){
   e.preventDefault();
   console.log(document.getElementById('username').value);
   console.log(document.getElementById('userEmail').value);
   username = document.getElementById('username').value;
   userEmail = document.getElementById('userEmail').value;
+  if(form === 'yes'){
+    formSubmit('yes');
+    return;
+  }
   questionTextChange(QuestionMsg);
 }
 
@@ -39,10 +51,9 @@ function questionTextChange(msg){
           <div class="recordStart" style="background-color: lightgrey;" onclick="">
           <i class="fas fa-video"></i> Record your video
           </div>
-          <div class="recordStart" style="background-color: lightgrey;" onclick="">
+          <label for="" style="margin: 0px;padding: 0px;font-size: 1rem;display:block;"><div class="recordStart" style="background-color: lightgrey;" onclick="">
                   <i class="fas fa-upload"></i> Upload your video
-                  </div>
-          <div>
+                  </div></label>
             You can practice before sending.
           </div>
         </div>
@@ -65,10 +76,9 @@ function questionTextChange(msg){
           <div class="recordStart" onclick="startFrame()">
           <i class="fas fa-video"></i> Record your video
           </div>
-          <div class="recordStart" onclick="">
+          <label for="video_file" style="margin: 0px;padding: 0px;font-size: 1rem;display:block;"><div class="recordStart">
                   <i class="fas fa-upload"></i> Upload your video
-                  </div>
-          <div>
+                  </div></label>
             You can practice before sending.
           </div>
         </div>
@@ -81,7 +91,6 @@ function questionTextChange(msg){
 
   /////end/////
 }
-
 
 function startFrame() {
   if(username === '' || userEmail === ''){
@@ -102,23 +111,12 @@ function startFrame() {
   addData.innerHTML = ` <div>
   
   <button id="record"></button>
-  <div id="counter" style="position: absolute;top: 80px;left: 0px;width: 100%;font-size: 2rem;color:white;">60</div>
+  <div id="counter" style="position: absolute;top: 20px;left: 0px;width: 100%;font-size: 2rem;color:white;">60</div>
   <button id="close"><i class="fas fa-times"></i></button>
   </div>
   <button id="play" style="display: none;" disabled>Play</button>
   <button id="download" style="display: none;" disabled>Download</button>
 <div>
-  
-<form
-id="upload"
-action="http://103.107.66.31:9006/upload-file"
-method="POST"
-enctype="multipart/form-data"
->
-<input id="video_file" hidden name="filename" type="file" accept="video/*"/>
-<label for="video_file" style="margin: 0px;" id="videoLabel" class="button"><i class="fas fa-cloud-upload-alt"></i></label>
-<input type="submit" hidden/>
-</form>
 </div>
 <video id="recorded" style="display: none;" autoplay loop controls playsinline></video>`;
   var mediaSource = new MediaSource();
@@ -126,10 +124,6 @@ enctype="multipart/form-data"
   var mediaRecorder;
   var recordedBlobs;
   var sourceBuffer;
-  var uploadFile = document.getElementById("video_file");
-  uploadFile.onchange = () => {
-    formSubmit("yes");
-  };
   var close = document.getElementById("close");
   close.onclick = () => formSubmit("no");
   var gumVideo = document.querySelector("video#gum");
@@ -161,6 +155,7 @@ enctype="multipart/form-data"
   function successCallback(stream) {
     console.log("getUserMedia() got stream: ", stream);
     window.stream = stream;
+    videoStream = stream;
     gumVideo.srcObject = stream;
   }
 
@@ -199,6 +194,7 @@ enctype="multipart/form-data"
   }
 
   function toggleRecording() {
+    clearInterval(intervalId);
     if (recordButton.innerHTML === "") {
       document.getElementById("userModal").style.display = "flex";
       var count = 4;
@@ -212,8 +208,8 @@ enctype="multipart/form-data"
           var recordCounter = document.getElementById("recordCounter");
           recordCounter.innerHTML = count;
           count--;
-        }, 2000);
-      }, 1000);
+        }, 1000);
+      }, 500);
       setTimeout(function () {
         startRecording();
         document.getElementById("userModal").style.display = "none";
@@ -237,7 +233,8 @@ enctype="multipart/form-data"
             downloadButton.disabled = false;
           }
         }, 1000);
-      }, 9000);
+      }, 4500);
+
     } else {
       stopRecording();
       recordButton.innerHTML = "";
@@ -333,7 +330,6 @@ enctype="multipart/form-data"
   </video>
   <img src="./ai-vid_GET/logo.png" class="logoImg"/>
   <div id="questionText" class="questionText">
-    
   </div>
 </div>
 <div id="readyState">
@@ -356,145 +352,9 @@ enctype="multipart/form-data"
     var superBuffer = new Blob(recordedBlobs, { type });
     readyVideo.src = window.URL.createObjectURL(superBuffer);
     yesChoice.onclick = () => afterSelection(superBuffer);
-  }
-
-  function yesClick(form) {
-    container.innerHTML += `<div class="confirmPage"><div class="confirmText">Uploading Data. Please wait ...</div></div>`;
-    console.log();
-    var fd = form.childNodes[1].value;
-
-    var fdata = new FormData(form);
-    var fd1;
-    for (var pair of fdata.entries()) {
-      fd1 = pair[1];
-    }
-    console.log(fd1);
-    console.log(fd);
-    var formdata = new FormData();
-    formdata.append("filename", fd1, fd);
-    // console.log(fd.get('#video_file'));
-    var requestOptions = {
-      method: "POST",
-      body: formdata,
-      redirect: "follow",
-      mode: "no-cors",
-    };
-    fetch("http://103.107.66.31:9008/upload-file", requestOptions)
-      .then((response) => response)
-      .then((result) => {
-        container.innerHTML +=
-          '<div class="confirmPage"><div class="confirmText">Thank you for uploading the video</div><div class="confirmChoice"><div id="noBtn" class="confirmBtn">Back</div></div></div>';
-
-        var noOption = document.getElementById("noBtn");
-
-        noOption.onclick = () => {
-          formSubmit("no");
-        };
-        return console.log(result);
-      })
-      .catch((error) => console.log("error", error));
-  }
-
-  function formSubmit(value) {
-    console.log(value);
-    if (value === "yes") {
-      var toUpload = document.getElementById("upload");
-      // toUpload.submit();
-      var fdata = new FormData(document.getElementById("upload"));
-      var fd;
-      for (var pair of fdata.entries()) {
-        fd = pair[1];
-      }
-      container.innerHTML = `
-    <div class="adminSide">
-    <video id="leftVideo" autoplay muted="muted" loop>
-      <source src="./ai-vid_GET/matt_1_muted.mp4" type="video/mp4">
-    </video>
-    <img src="./ai-vid_GET/logo.png" class="logoImg"/>
-    <div id="questionText" class="questionText">
-      
-    </div>
-  </div>
-  <div id="readyState">
-    <video height="100%" width="100%" id="readyVideo" autoplay loop>
-    </video>
-    <div id="sendMessage">
-      <div>Ready to send?</div>
-      <div id="sendMessageChoice">
-        <div id="choiceYes">Yes</div>
-        <div id="choiceNo">No</div>
-      </div>
-    </div>
-  </div>
-    `;
-      var readyVideo = document.getElementById("readyVideo");
-      readyVideo.src = window.URL.createObjectURL(fd);
-      var yesChoice = document.getElementById("choiceYes");
-      console.log(yesChoice);
-      console.log(fd);
-      var superBuffer = new Blob([fd], { type: fd.type });
-      yesChoice.onclick = () => yesClick(toUpload);
-      // yesChoice.onClick = () => {
-      //   console.log('Hello');
-      //   document.body.appendChild(toUpload);
-      //   toUpload.submit();
-      // };
-      var noChoice = document.getElementById("choiceNo");
-      noChoice.onclick = () => formSubmit("no");
-    } else if (value === "yes123") {
-      download();
-      container.innerHTML = `
-    <div class="adminSide">
-        <video id="leftVideo" autoplay muted="muted" loop>
-          <source src="./ai-vid_GET/matt_1_muted.mp4" type="video/mp4">
-        </video>
-        <img src="./ai-vid_GET/logo.png" class="logoImg"/>
-        <div id="questionText" class="questionText">
-          
-        </div>
-      </div>
-      <div id="userSide">
-        <div class="startVideo">
-          <div class="recordStart" onclick="startFrame()">
-            <i class="fas fa-video"></i> Record your video
-          </div>
-          <div class="recordStart" onclick="">
-                  <i class="fas fa-upload"></i> Upload your video
-                  </div>
-          <div>
-            You can practice before sending.
-          </div>
-        </div>
-      </div>
-    `;
-      document.getElementById("questionText").innerHTML = QuestionMsg;
-    } else {
-      container.innerHTML = `
-    <div class="adminSide">
-        <video id="leftVideo" autoplay muted="muted" loop>
-          <source src="./ai-vid_GET/matt_1_muted.mp4" type="video/mp4">
-        </video>
-        <img src="./ai-vid_GET/logo.png" class="logoImg"/>
-        <div id="questionText" class="questionText">
-          
-        </div>
-      </div>
-      <div id="userSide">
-        <div class="startVideo">
-          <div class="recordStart" onclick="startFrame()">
-          <i class="fas fa-video"></i> Record your video
-          </div>
-          <div class="recordStart" onclick="">
-                  <i class="fas fa-upload"></i> Upload your video
-                  </div>
-          <div>
-            You can practice before sending.
-          </div>
-        </div>
-      </div>
-    `;
-      document.getElementById("questionText").innerHTML = QuestionMsg;
-    }
+    videoStream.getTracks().forEach(function(track) {
+      track.stop();
+    });
   }
 
   function play() {
@@ -519,4 +379,165 @@ enctype="multipart/form-data"
   }
 }
 
+function formSubmit(value) {
+  console.log(value);
+  if (value === "yes") {
+    var toUpload = document.getElementById("upload");
+    // toUpload.submit();
+    var fdata = new FormData(document.getElementById("upload"));
+    var fd;
+    for (var pair of fdata.entries()) {
+      fd = pair[1];
+    }
+    if(username === '' || userEmail === ''){
+      container.innerHTML = `
+      <div class="adminSide">
+      <video id="leftVideo" class="video-aiexplore" autoplay muted="muted" loop>
+        <source src="./ai-vid_GET/matt_1_muted.mp4" type="video/mp4">
+      </video>
+      <img src="./ai-vid_GET/logo.png" class="logoImg"/>
+      <div id="questionText" class="questionText">
+        
+      </div>
+    </div>
+    <div id="userSide">
+<div class="startVideo">
+<div><input id="username" type="text" placeholder="Username" required/></div>
+<div><input id="userEmail" type="email" placeholder="User Email" required/></div>
+<div><button id="detailSubmitButton" type="button" onclick="detailSubmit(event,'yes')">Submit</button></div>
+</div>
+</div>
+      `
+      document.getElementById("questionText").innerHTML = QuestionMsg;
+    }else{
+      container.innerHTML = `
+  <div class="adminSide">
+  <video id="leftVideo" autoplay muted="muted" loop>
+    <source src="./ai-vid_GET/matt_1_muted.mp4" type="video/mp4">
+  </video>
+  <img src="./ai-vid_GET/logo.png" class="logoImg"/>
+  <div id="questionText" class="questionText">
+    
+  </div>
+</div>
+<div id="readyState">
+  <video height="100%" width="100%" id="readyVideo" autoplay loop>
+  </video>
+  <div id="sendMessage">
+    <div>Ready to send?</div>
+    <div id="sendMessageChoice">
+      <div id="choiceYes">Yes</div>
+      <div id="choiceNo">No</div>
+    </div>
+  </div>
+</div>
+  `;
+    var readyVideo = document.getElementById("readyVideo");
+    readyVideo.src = window.URL.createObjectURL(fd);
+    var yesChoice = document.getElementById("choiceYes");
+    console.log(yesChoice);
+    console.log(fd);
+    var superBuffer = new Blob([fd], { type: fd.type });
+    videoStream.getTracks().forEach(function(track) {
+      track.stop();
+    });
+    yesChoice.onclick = () => yesClick(toUpload);
+    var noChoice = document.getElementById("choiceNo");
+    noChoice.onclick = () => formSubmit("no");
+    }
+  } else if (value === "yes123") {
+    download();
+    container.innerHTML = `
+  <div class="adminSide">
+      <video id="leftVideo" autoplay muted="muted" loop>
+        <source src="./ai-vid_GET/matt_1_muted.mp4" type="video/mp4">
+      </video>
+      <img src="./ai-vid_GET/logo.png" class="logoImg"/>
+      <div id="questionText" class="questionText">
+        
+      </div>
+    </div>
+    <div id="userSide">
+      <div class="startVideo">
+        <div class="recordStart" onclick="startFrame()">
+          <i class="fas fa-video"></i> Record your video
+        </div>
+        <label for="video_file" style="margin: 0px;padding: 0px;font-size: 1rem;display:block;"><div class="recordStart">
+                <i class="fas fa-upload"></i> Upload your video
+                </div></label>
+          You can practice before sending.
+        </div>
+      </div>
+    </div>
+  `;
+    document.getElementById("questionText").innerHTML = QuestionMsg;
+  } else {
+    container.innerHTML = `
+  <div class="adminSide">
+      <video id="leftVideo" autoplay muted="muted" loop>
+        <source src="./ai-vid_GET/matt_1_muted.mp4" type="video/mp4">
+      </video>
+      <img src="./ai-vid_GET/logo.png" class="logoImg"/>
+      <div id="questionText" class="questionText">
+        
+      </div>
+    </div>
+    <div id="userSide">
+      <div class="startVideo">
+        <div class="recordStart" onclick="startFrame()">
+        <i class="fas fa-video"></i> Record your video
+        </div>
+        <label for="video_file" style="margin: 0px;padding: 0px;font-size: 1rem;display:block;"><div class="recordStart">
+                <i class="fas fa-upload"></i> Upload your video
+                </div></label>
+          You can practice before sending.
+        </div>
+      </div>
+    </div>
+  `;
+    document.getElementById("questionText").innerHTML = QuestionMsg;
+    var uploadFile = document.getElementById("video_file");
+  uploadFile.onchange = () => {
+    console.log('uploaded');
+    formSubmit("yes");
+};
+  }
+}
+
+function yesClick(form) {
+  container.innerHTML += `<div class="confirmPage"><div class="confirmText">Uploading Data. Please wait ...</div></div>`;
+  console.log();
+  var fd = form.childNodes[1].value;
+
+  var fdata = new FormData(form);
+  var fd1;
+  for (var pair of fdata.entries()) {
+    fd1 = pair[1];
+  }
+  console.log(fd1);
+  console.log(fd);
+  var formdata = new FormData();
+  formdata.append("filename", fd1, fd);
+  // console.log(fd.get('#video_file'));
+  var requestOptions = {
+    method: "POST",
+    body: formdata,
+    redirect: "follow",
+    mode: "no-cors",
+  };
+  fetch("http://103.107.66.31:9008/upload-file", requestOptions)
+    .then((response) => response)
+    .then((result) => {
+      container.innerHTML +=
+        '<div class="confirmPage"><div class="confirmText">Thank you for uploading the video</div><div class="confirmChoice"><div id="noBtn" class="confirmBtn">Back</div></div></div>';
+
+      var noOption = document.getElementById("noBtn");
+
+      noOption.onclick = () => {
+        formSubmit("no");
+      };
+      return console.log(result);
+    })
+    .catch((error) => console.log("error", error));
+}
 // window.isSecureContext could be used for Chrome
